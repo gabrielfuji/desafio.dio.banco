76b2171c8efb2abe8c8527426a25cf8ca87981b2
Fuji <gabrielfujihara21@gmail.com>
2022-04-03 22:06:32 -0300

    first commit

---
+++ src/com/company/contaCorrente.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class contaCorrente {
+    public int agencia;
+    public int numero;
+    public int saldo;
+
+    public void sacar() {
+
+    }
+    public void depositar(){
+
+    }
+    public void transferir(){
+
+    }
+}
+
---
+++ src/com/company/contaPoupanca.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class contaPoupanca {
+    public int agencia;
+    public int numero;
+    public int saldo;
+
+    public void sacar() {
+
+    }
+    public void depositar(){
+
+    }
+    public void transferir(){
+
+    }
+}
